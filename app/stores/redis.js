0013f1b79c27f566b05ba675e0421c95033546b5
keith <keith@70kft.com>
2015-03-24 10:41:55 -0500

    trying redistogo

--- app/stores/redis.js
+++ app/stores/redis.js
@@ -1,3 +1,11 @@
-var client = require('redis').createClient();
+var client;
+if (process.env.REDISTOGO_URL) {
+  var rtg = require('url').parse(process.env.REDISTOGO_URL);
+  client = require('redis').createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(':')[1]);
+}
+else {
+  client = require('redis').createClient();  
+}
 
 module.exports = client;
